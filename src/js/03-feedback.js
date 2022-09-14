faf64c59fcac970e1f06d8883e4213def64e6182
Brudja <n0669378587@gmail.com>
2022-09-14 17:02:24 +0300

    Upd 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,6 +9,15 @@ const localKey = "feedback-form-state"
 form.addEventListener("input", Throttle(storageFromData, 500));
 form.addEventListener("submit", onFormSubmit);
 
+
+window.addEventListener('load', checkStorage);
+function checkStorage() {
+  if (!localStorage.getItem(localKey)) return;
+  const formValue = JSON.parse(localStorage.getItem(localKey));
+  email.value = formValue.email;
+  message.value = formValue.message;
+}
+
 function onFormSubmit(event){
     event.preventDefault();
     const {email, message}=event.currentTarget.elements
