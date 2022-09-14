f90ff4c19a241cf298d32e9759645a33807418e8
Brudja <n0669378587@gmail.com>
2022-09-14 16:17:08 +0300

    дз 8 часть 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import Throttle from "lodash.throttle"
+
+    const form = document.querySelector(".feedback-form");
+    const email = form.querySelector("[name=email]");
+    const message = form.querySelector("[name=message]");
+
+const localKey = "feedback-form-state"
+
+form.addEventListener("input", Throttle(storageFromData, 500));
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event){
+    event.preventDefault();
+    const {email, message}=event.currentTarget.elements
+    console.dir({email: email.value, message:message.value})
+    
+    localStorage.removeItem(localKey);
+    email.value = "";
+    message.value = "";
+
+}
+
+function storageFromData(event){
+    const formValue = {email:"", message:""};
+
+    if(localStorage.getItem(localKey)){
+        Object.assign(formValue, JSON.parse(localStorage.getItem(localKey)))
+    }
+
+    formValue[event.target.name] = event.target.value;
+
+
+
+    localStorage.setItem(localKey, JSON.stringify(formValue))
+}
 
